cc0d60605fa1833f2033c9f5274bdb84da5c67bf
nukul4r <phm@stfu.de>
2013-09-19 21:03:52 +0200

    link type & btc

--- MucWetter/src/main/java/com/nukul4r/mucwetter/ContentType.java
+++ MucWetter/src/main/java/com/nukul4r/mucwetter/ContentType.java
@@ -4,5 +4,6 @@ package com.nukul4r.mucwetter;
  * Created by phm on 19.09.13.
  */
 public enum ContentType {
-
+    IMAGE,
+    HTML
 }
--- MucWetter/src/main/java/com/nukul4r/mucwetter/MainActivity.java
+++ MucWetter/src/main/java/com/nukul4r/mucwetter/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends Activity {
         registerButton(R.id.btnWrf);
         registerButton(R.id.btnMm);
         registerButton(R.id.btnNsr);
+        registerButton(R.id.btnHome);
     }
 
     public void registerButton(final int id) {
@@ -35,14 +36,30 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View arg0) {
                 String htmlString = "";
+                ContentType type = null;
                 try {
                     htmlString = getHtmlString(id);
+                    type = getContentType(id);
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                ((WebView) findViewById(R.id.webView)).loadData(htmlString, "text/html", "UTF-8");
+                if (ContentType.IMAGE.equals(type)) {
+                    ((WebView) findViewById(R.id.webView)).loadData(htmlString, "text/html", "UTF-8");
+                } else if (ContentType.HTML.equals(type)) {
+                    ((WebView) findViewById(R.id.webView)).loadUrl(findViewById(id).getTag().toString().substring(2));
+                }
             }
         });
+    }
+
+    private ContentType getContentType(int id) {
+        String type = findViewById(id).getTag().toString().substring(0,1);
+        if (type.equals("i")) {
+            return ContentType.IMAGE;
+        } else {
+            return ContentType.HTML;
+        }
     }
 
     public String getHtmlString(int id) throws IOException {
@@ -51,7 +68,7 @@ public class MainActivity extends Activity {
         is.read(buffer);
         is.close();
         String s = new String(buffer);
-        s = s.replace("url", findViewById(id).getTag().toString());
+        s = s.replace("url", findViewById(id).getTag().toString().substring(2));
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             s = s.replace("height", "width");
         }
